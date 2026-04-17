7522c64113ca6e47adf38e36bb67b01bb4ffcd07
Maria Eduarda Gomes <166930239+memariaa@users.noreply.github.com>
2026-04-17 12:11:02 -0300

    fix

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,38 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsuarioService } from './services/usuario.services';
+import { UsuarioService } from './services/usuario.service';
 import { UsuarioController } from './controller/usuario.controller';
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsEmail, MinLength } from 'class-validator';
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
-import { Produto } from '../produto/entities/produto.entity';
 
-@Entity({ name: 'tb_usuarios' })
-export class Usuario {
-  @PrimaryGeneratedColumn()
-  @ApiProperty()
-  id: number;
-
-  @IsNotEmpty()
-  @Column({ length: 255, nullable: false })
-  @ApiProperty()
-  nome: string;
-
-  @IsEmail()
-  @Column({ length: 255, nullable: false })
-  @ApiProperty({ example: 'email@email.com.br' })
-  usuario: string;
-
-  @IsNotEmpty()
-  @MinLength(8)
-  @Column({ length: 255, nullable: false })
-  @ApiProperty()
-  senha: string;
-
-  @ApiProperty()
-  @OneToMany(() => Produto, (produto) => produto.usuario)
-  produto: Produto[];
-}
 @Module({
   imports: [TypeOrmModule.forFeature([Usuario])],
   providers: [UsuarioService],
